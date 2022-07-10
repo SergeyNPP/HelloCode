0dbb42d7a903b5cdf1dcc3c49faa6a97af90cce4
Sergey_NPP <budanoff.2011@yandex.ru>
2022-07-10 13:12:38 +0400

    add Example 006 Base

---
+++ Examples/Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 3;
+int b= 4;
+int c= 1;
+int d = 8;
+int e = 7;
+
+int max = a;
+if (a> max) max = a;
+if (b> max) max = b;
+if (c> max) max = c;
+if (d> max) max = d;
+if (e> max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
